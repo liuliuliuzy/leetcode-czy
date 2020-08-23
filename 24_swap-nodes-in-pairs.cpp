ba92a3d4e38ab315a50b9cb2770888508d747f30
liuziyang <1872405512@qq.com>
2020-08-23 12:07:09 +0800

    递归解决24

--- 24_swap-nodes-in-pairs.cpp
+++ 24_swap-nodes-in-pairs.cpp
@@ -18,13 +18,44 @@ struct ListNode {
 
 class Solution {
 public:
-    
-    void swapPairs(ListNode* head) {
+    void swapHelper(ListNode*& head){
         ListNode* tmp1 = head->next->next;
         ListNode* tmp2 = head;
         head = head->next;
         tmp2->next = tmp1;
         head->next = tmp2;
+    }
+    //错误尝试
+    ListNode* swapPairsV2(ListNode* head) {
+        ListNode* ans;
+        bool flag = true;
+        while (head && head->next)
+        {
+            printf("%s %d\n", "1", head->val);
+            swapHelper(head);
+            printf("%s %d %d\n", "21", head->val, head->next->val);
+            if(flag){
+                ans = head;
+                flag = false;
+            }
+            head = head->next->next;
+            if(head){
+                printf("%s %d\n", "213", head->val);
+            }  
+        }
+        return ans;
+    }
+    ListNode* swapPairs(ListNode* head){
+        //终止条件
+        if(!head || !head->next){
+            return head;
+        }
+        ListNode* t1 = head;
+        ListNode* t2 = head->next->next;
+        head = head->next;
+        head->next = t1;
+        t1->next = swapPairs(t2);
+        return head;
     }
     void printLinkedList(ListNode* head) {
         ListNode* tmp = head;
@@ -43,11 +74,15 @@ int main()
     ListNode s2(2);
     ListNode s3(3);
     ListNode s4(4);
+    ListNode s5(5);
+    ListNode s6(6);
     s.next = (ListNode*)(&s2);
     s2.next = (ListNode*)(&s3);
     s3.next = (ListNode*)(&s4);
+    s4.next = (ListNode*)(&s5);
+    s5.next = (ListNode*)(&s6);
     so.printLinkedList(&s);
-    so.swapPairs(&s);
-    so.printLinkedList(&s);
+    so.printLinkedList(so.swapPairs(&s));
+    // so.printLinkedList(head);
     return 0;
 }
