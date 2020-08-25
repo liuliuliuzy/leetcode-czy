c48af36e630e918e42fefa3cae736c25e8d93763
liuziyang <1872405512@qq.com>
2020-08-25 10:18:01 +0800

    第19题，扫描两次

--- basicInclude.h
+++ basicInclude.h
@@ -7,3 +7,10 @@
 #include <cmath>
 
 using namespace std;
+
+//Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
