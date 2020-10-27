c9740cf5b44f8b701d4eb6ee34931e3655766c60
liuliuliuzy <1872405512@qq.com>
2020-10-27 16:48:30 +0800

    test1

--- 144_binary-tree-preorder-traversal.cpp
+++ 144_binary-tree-preorder-traversal.cpp
@@ -13,12 +13,17 @@ struct TreeNode {
 
 class Solution {
 public:
+    // Recursive version
     void helper (TreeNode* root, vector<int> &res){
         if(!root) return ;
         // 调整对当前节点的操作语句放置的位置，即可分别完成二叉树的前/中/后序遍历
         res.push_back(root->val);
         helper(root->left, res);
         helper(root->right, res);
+    }
+    // Iteration version
+    vector<int> mapAllNodes(TreeNode* root){
+
     }
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> res;
