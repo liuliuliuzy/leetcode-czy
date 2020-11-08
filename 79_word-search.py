990145d63e2b3461096ef722bfcf7f5b90fd91c8
liuliuliuzy <1872405512@qq.com>
2020-11-08 10:50:10 +0800

    added 79

--- 79_word-search.py
+++ 79_word-search.py
@@ -2,31 +2,50 @@ from typing import List
 
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        # 四个方向
+        directions = [(0,1), (1,0), (-1,0), (0,-1)]
+        def judge(i: int, j: int, k: int) -> bool:
+            # 如果不等，则不可能匹配成功
+            if board[i][j] != word[k]:
+                return False
+
+            # 如果已经匹配完了最后一个字符
+            if k == len(word)-1:
+                return True
+            
+            result = False
+            visited.add((i, j))
+            for di, dj in directions:
+                newi, newj = i+di, j+dj
+                if 0 <= newi < m and 0 <= newj < n:
+                    # 如果没被访问过
+                    if (newi, newj) not in visited:
+                        if judge(newi, newj, k+1):
+                            result = True
+                            break
+            
+            visited.remove((i, j))
+            return result            
+
         # 遍历回溯
-        p = 0 # 字符串字符指针
+        # p = 0 # 字符串字符指针
         m = len(board)
         n = len(board[0])
+        visited = set()
         for i in range(m):
             for j in range(n):
-                if board[i][j] != word[p]:
-                    continue
-                # 执行回溯算法
-                for t in range
-                for disI, disJ in [(1,1), (1,-1), (-1,1), (-1,-1)]:
-                    p += 1
-                    if i+disI >= 0 and i+disI < n and j+disJ >= 0 and j+disJ < n and board[i][j] == word[p]:
-
-
+                if judge(i, j, 0):
+                    return True
         return False
 
 if __name__ == "__main__":
     board =[
-        ['A','B','C','E'],s
+        ['A','B','C','E'],
         ['S','F','C','S'],
         ['A','D','E','E']
     ]
-    s = "ABC"
+    s = "ABCCED"
     solu = Solution()
-    solu.exist(board, s)
+    print(solu.exist(board, s))
 
     
--- basicInclude.h
+++ basicInclude.h
@@ -1,16 +1,18 @@
+//标准库
 #include <cstdio>
 #include <cstdlib>
 #include <algorithm>
 #include <string>
 #include <iostream>
 #include <cmath>
-//常用的数据结构
+//常用的标准容器-数据结构
 #include <vector>
 #include <stack>
 #include <queue>
 #include <set>
 #include <unordered_set>
 
+//使用标准命名空间
 using namespace std;
 
 //Definition for singly-linked list.
