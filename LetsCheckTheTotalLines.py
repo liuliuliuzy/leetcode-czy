0a6b889062f5f890cc2f1c4bcaba879ded54c6de
liuliuliuzy <1872405512@qq.com>
2020-11-12 21:04:34 +0800

    merge

--- LetsCheckTheTotalLines.py
+++ LetsCheckTheTotalLines.py
@@ -8,6 +8,6 @@ files = os.listdir('.')
 for file in files:
     if file.startswith('.'):
         continue
-    totalLines += len(open(file, 'r').readlines())
+    totalLines += len(open(file, 'r', encoding='UTF-8').readlines())
 end = time.time()
 print('Look Hooooow many lines of codes have you wrote\n===============================\nTotal Lines Count: {} in {:.2}s\n==============================='.format(totalLines-13, end-start))
