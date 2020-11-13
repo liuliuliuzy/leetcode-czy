d92c0f30f9ff8c64c8339348b43af19aa3d8dc03
liuliuliuzy <1872405512@qq.com>
2020-11-13 19:45:01 +0800

    solved 328 easy

--- 328_odd-even-linked-list.py
+++ 328_odd-even-linked-list.py
@@ -16,37 +16,32 @@ class Solution:
         调整链表
         注意链表数据结构的特殊性
         '''
+        if not head or not head.next:
+            return head
         oddTmp = head
-        evenNode = evenTmp = None
-        if head.next:
-            evenNode = evenTmp = head.next
-        # while oddTmp != None and oddTmp.next != None and oddTmp.next.next != None:
-        #     # if oddTmp:
-        #     # if oddTmp.next and oddTmp.next.next:
-        #     oddTmp.next =  oddTmp.next.next
-        #     oddTmp = oddTmp.next
-        # while evenTmp != None and evenTmp.next != None and evenTmp.next.next != None:
-        #     # if evenTmp.next and evenTmp.next.next:
-        #     evenTmp.next = evenTmp.next.next
-        #     evenTmp = evenTmp.next
-        while (oddTmp and oddTmp.next and oddTmp.next.next) or (evenTmp and evenTmp.next and evenTmp.next.next):
+        evenNode = evenTmp = head.next
+        while (oddTmp.next) or (evenTmp.next):
             if oddTmp.next and oddTmp.next.next:
                 oddTmp.next =  oddTmp.next.next
-                oddTmp = oddTmp.next           
+                oddTmp = oddTmp.next
+            else:
+                break        
             if evenTmp.next and evenTmp.next.next:
                 evenTmp.next = evenTmp.next.next
                 evenTmp = evenTmp.next
-        oddTmp.next = evenNode
-            
-
+            else:
+                evenTmp.next = None
+        oddTmp.next = evenNode      
+        return head
 
 if __name__ == "__main__":
-    a5 = ListNode(5)
-    a4 = ListNode(4, a5)
-    a3 = ListNode(3, a4)
+    # a5 = ListNode(5)
+    # a4 = ListNode(4)
+    a3 = ListNode(3)
     a2 = ListNode(2, a3)
     a1 = ListNode(1, a2)
     a1.printSelf()
+    print("")
     s = Solution()
     s.oddEvenList(a1)
     a1.printSelf()
