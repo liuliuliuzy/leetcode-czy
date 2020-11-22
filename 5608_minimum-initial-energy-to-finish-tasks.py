3126166fbdd619f4a7a456b6659a5373a144e3e3
liuziyang <1872405512@qq.com>
2020-11-22 13:30:38 +0800

    5608修改了一下

--- 5608_minimum-initial-energy-to-finish-tasks.py
+++ 5608_minimum-initial-energy-to-finish-tasks.py
@@ -25,10 +25,24 @@ class Solution:
             # res = max(res + tasks[i][0], tasks[i][1])
             # print(res)
         return res
-    
+    def minimumEffortII(self, tasks: List[List[int]]) -> int:
+        tasks.sort(key = lambda x: x[1] - x[0])
+        res = 0
+        while tasks:
+            for i in range(len(tasks)):
+                if res + tasks[i][0] >= tasks[i][1]:
+                    res = res + tasks[i][0]
+                    del tasks[i]
+                    break
+                else:
+                    if i == len(tasks) - 1:
+                        res = tasks[0][1]
+                        del tasks[0]
+
+        return res
 
 
 if __name__ == "__main__":
     test = [[1,7],[2,8],[3,9],[4,10],[5,11],[6,12]]
     s = Solution()
-    print(s.minimumEffort(test))
+    print(s.minimumEffortII(test))
