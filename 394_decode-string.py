0dc1fc7f2a9c8ae8251878bef8e0ed6cd224ae6e
liuliuliuzy <1872405512@qq.com>
2020-11-28 21:06:49 +0800

    394 interesting

--- 394_decode-string.py
+++ 394_decode-string.py
@@ -2,23 +2,41 @@ class Solution:
     def decodeString(self, s: str) -> str:
         '''
         唯一的问题：解决编码字符串的嵌套处理
+        双栈，一个存数字，一个存待乘的字符串
+        看到用栈的提示，一时没有想出来，后来居然自己想出来了，还不错(*^_^*)
         '''
-        res = ""
         specials = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '[', ']']
         st1 = []  # for number
-        st2 = []  # for '['
-        for i in range(len(s)):
+        st2 = [""] # for string
+        
+        i = 0
+        while i < len(s):
+            # 如果是字母
             if s[i] not in specials:
-                res += s[i]
+                st2[-1] += s[i]
+                i += 1
                 continue
+            # 如果是特殊字符
             if s[i] == '[':
-                st2.append(i)
+                st2.append("")
             elif s[i] == ']':
-                
+                tmpSt = int(st1[-1]) * st2[-1]
+                st2.pop()
+                st2[-1] += tmpSt
+                st1.pop()
+            else:
+                st1.append(s[i])
+                i += 1
+                while 48 <= ord(s[i]) <= 57:
+                    st1[-1] += s[i]
+                    i += 1
+                continue
+            i += 1
+        return st2[0]
 
 
         return res
 if __name__ == "__main__":
     s = Solution()
-    encodedString = ""
+    encodedString = "dff"
     print(s.decodeString(encodedString))
